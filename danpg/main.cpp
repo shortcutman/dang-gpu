f83f2b595a45b2528d37bcd738a0ff569c07925e
Daniel Burke <danieljburke@me.com>
2023-05-02 21:22:26 +1000

    Initial Commit

---
+++ danpg/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  danpg
+//
+//  Created by Daniel Burke on 2/5/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
