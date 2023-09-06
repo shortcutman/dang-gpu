a6a51e3be91bea4736f5c58ef6930ee8fb1a4b0c
Daniel Burke <danieljburke@me.com>
2023-09-06 22:17:08 +1000

    Added report of decode execution time.

--- danpg/main.cpp
+++ danpg/main.cpp
@@ -11,6 +11,16 @@
 #include "jpeg.hpp"
 #include "colour.hpp"
 
+void runFuncTimed(std::function<void(void)> func) {
+    std::cout << "Jpeg decode start" << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    
+    func();
+    
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "Jpeg decode duration: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+}
+
 int main(int argc, const char * argv[]) {
     
     std::fstream f("/Users/daniel/Projects.nosync/danpg/danpg/danpg/image2.jpg");
@@ -18,9 +28,13 @@ int main(int argc, const char * argv[]) {
     if (f.bad()) {
         std::cout << "unable to open" << std::endl;
     } else {
+        image::Jpeg jpeg;
+        
         std::cout << "opened" << std::endl;
         
-        image::Jpeg jpeg(f);
+        runFuncTimed([&]() {
+            jpeg = image::Jpeg(f);
+        });
         
         image::writeOutPPM("/private/tmp/jpeg.ppm", jpeg._x, jpeg._y, jpeg._image);
     }
