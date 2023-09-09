b2dcd0324848d58a32274ed4972cfb51797e67fc
Daniel Burke <danieljburke@me.com>
2023-09-09 19:16:55 +1000

    Unit tests for table based algorithms.

--- danpg-tests/idct_test.cpp
+++ danpg-tests/idct_test.cpp
@@ -38,6 +38,32 @@ TEST(IDCTTest, InputAndOutputOfLumaFromTestImageFloat) {
     EXPECT_EQ(result, output);
 }
 
+TEST(IDCTTest, InputAndOutputOfLumaFromTestImageFloatTable) {
+    image::DataUnit input = {
+        -430, -10, 20, 0, 0, 0, 0, 0,
+        20, 0, 0, 0, 0, 0, 0, 0,
+        -20, 10, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+    };
+    image::DataUnit output = {
+        -49, -51, -54, -56, -56, -55, -52, -50,
+        -49, -51, -53, -55, -55, -53, -50, -48,
+        -49, -51, -53, -54, -53, -50, -46, -44,
+        -50, -51, -53, -53, -52, -48, -45, -42,
+        -51, -53, -54, -55, -53, -50, -46, -43,
+        -53, -55, -57, -58, -57, -54, -50, -48,
+        -55, -57, -59, -61, -61, -58, -56, -53,
+        -56, -58, -61, -63, -63, -62, -59, -57
+    };
+    
+    auto result = image::idct_float_table(input);
+    EXPECT_EQ(result, output);
+}
+
 TEST(IDCTTest, InputAndOutputOfLumaFromTestImageInteger) {
     image::DataUnit input = {
         -430, -10, 20, 0, 0, 0, 0, 0,
@@ -61,6 +87,33 @@ TEST(IDCTTest, InputAndOutputOfLumaFromTestImageInteger) {
     };
     
     auto result = image::idct_int(input);
+    
+    EXPECT_EQ(result, output);
+}
+
+TEST(IDCTTest, InputAndOutputOfLumaFromTestImageIntegerTable) {
+    image::DataUnit input = {
+        -430, -10, 20, 0, 0, 0, 0, 0,
+        20, 0, 0, 0, 0, 0, 0, 0,
+        -20, 10, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0
+    };
+    image::DataUnit output = {
+        -48, -51, -53, -56, -56, -54, -52, -50,
+        -48, -50, -53, -55, -54, -52, -49, -47,
+        -48, -50, -52, -53, -52, -49, -46, -43,
+        -49, -50, -52, -53, -51, -48, -44, -41,
+        -50, -52, -53, -54, -52, -49, -45, -42,
+        -52, -54, -56, -57, -56, -53, -49, -47,
+        -54, -56, -58, -60, -60, -58, -55, -53,
+        -55, -57, -60, -63, -63, -61, -58, -57
+    };
+    
+    auto result = image::idct_int_table(input);
     
     EXPECT_EQ(result, output);
 }
