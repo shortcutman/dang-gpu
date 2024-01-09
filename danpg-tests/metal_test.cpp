71151d2de86c68dbea623c863f6a8049b47b65a1
Daniel Burke <danieljburke@me.com>
2024-01-10 10:42:08 +1100

    Basic test for running a metal compute shader

---
+++ danpg-tests/metal_test.cpp
@@ -0,0 +1,106 @@
+//
+//  metal_test.cpp
+//  danpg-tests
+//
+//  Created by Daniel Burke on 10/1/2024.
+//
+
+#include <gtest/gtest.h>
+#include <stdexcept>
+#include <string>
+#include <strstream>
+
+#include <Foundation/Foundation.hpp>
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
+
+class MetalTest : public ::testing::Test {
+protected:
+    NS::SharedPtr<NS::AutoreleasePool> _pool;
+    NS::SharedPtr<MTL::Device> _metalDevice;
+    NS::SharedPtr<MTL::CommandQueue> _commandQueue;
+    
+    void SetUp() override {
+        _pool = NS::TransferPtr(NS::AutoreleasePool::alloc()->init());
+        _metalDevice = NS::TransferPtr(MTL::CreateSystemDefaultDevice());
+        ASSERT_TRUE(_metalDevice);
+        
+        _commandQueue = NS::TransferPtr(_metalDevice->newCommandQueue());
+        ASSERT_TRUE(_commandQueue);
+    }
+    
+    void TearDown() override {
+        _metalDevice.reset();
+        _pool.reset();
+    }
+    
+    NS::SharedPtr<MTL::ComputePipelineState> newPSOFromFuncName(const char* functionName) {
+        auto defaultLib = _metalDevice->newDefaultLibrary();
+        EXPECT_TRUE(defaultLib) << "No default metal lib";
+        
+        auto function = defaultLib->newFunction(NS::String::string(functionName, NS::ASCIIStringEncoding));
+        EXPECT_TRUE(function);
+        
+        NS::Error* error = nullptr;
+        
+        auto functionPSO = NS::TransferPtr(_metalDevice->newComputePipelineState(function, &error));
+        EXPECT_TRUE(functionPSO);
+        
+        return functionPSO;
+    }
+};
+
+namespace {
+
+const unsigned int arrayLength = 1 << 24;
+const unsigned int bufferSize = arrayLength * sizeof(float);
+
+void generateRandomFloatData(MTL::Buffer* buffer) {
+    float* data = reinterpret_cast<float*>(buffer->contents());
+    
+    for (unsigned int index = 0; index < arrayLength; index++) {
+        data[index] = (float)rand() / (float)(RAND_MAX);
+    }
+}
+
+}
+
+TEST_F(MetalTest, AddArraysIsolatedTest) {
+    auto pso = newPSOFromFuncName("add_arrays");
+    EXPECT_TRUE(pso);
+    
+    NS::SharedPtr<MTL::Buffer> bufferA = NS::TransferPtr(_metalDevice->newBuffer(bufferSize, MTL::ResourceStorageModeShared));;
+    NS::SharedPtr<MTL::Buffer> bufferB = NS::TransferPtr(_metalDevice->newBuffer(bufferSize, MTL::ResourceStorageModeShared));;
+    NS::SharedPtr<MTL::Buffer> bufferResult = NS::TransferPtr(_metalDevice->newBuffer(bufferSize, MTL::ResourceStorageModeShared));;
+    
+    generateRandomFloatData(bufferA.get());
+    generateRandomFloatData(bufferB.get());
+    
+    auto commandBuffer = _commandQueue->commandBuffer();
+    assert(commandBuffer != nullptr);
+    
+    auto computeEncoder = commandBuffer->computeCommandEncoder();
+    assert(computeEncoder != nullptr);
+    
+    computeEncoder->setComputePipelineState(pso.get());
+    computeEncoder->setBuffer(bufferA.get(), 0, 0);
+    computeEncoder->setBuffer(bufferB.get(), 0, 1);
+    computeEncoder->setBuffer(bufferResult.get(), 0, 2);
+    
+    auto gridSize = MTL::Size(arrayLength, 1, 1);
+    
+    auto threadGroupSize = pso->maxTotalThreadsPerThreadgroup();
+    if (threadGroupSize > arrayLength) {
+        threadGroupSize = arrayLength;
+    }
+    
+    auto threadGroupSizeObj = MTL::Size(threadGroupSize, 1, 1);
+    
+    computeEncoder->dispatchThreads(gridSize, threadGroupSizeObj);
+    computeEncoder->endEncoding();
+    
+    commandBuffer->commit();
+    commandBuffer->waitUntilCompleted();
+}
+
+
---
+++ libdanpg/add_arrays.metal
@@ -0,0 +1,16 @@
+//
+//  File.metal
+//  danpg-tests
+//
+//  Created by Daniel Burke on 10/1/2024.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+kernel void add_arrays(device const float* inA,
+                       device const float* inB,
+                       device float* result,
+                       uint index [[thread_position_in_grid]]) {
+    result[index] = inA[index] + inB[index];
+}
